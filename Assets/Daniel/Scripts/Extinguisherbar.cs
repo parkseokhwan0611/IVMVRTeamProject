5c95ec82cd9ef8494994f78c9107cef15a28f4a5
holidayfanboy <mkdaniel2020@gmail.com>
2024-12-08 16:04:21 +0900

    게임 스타트

---
+++ Assets/Daniel/Scripts/Extinguisherbar.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Extinguisherbar : MonoBehaviour
+{
+    [SerializeField] private Slider bar;
+    private float maxHP = 180;
+    private float curHP = 180;
+    float imsi;
+
+    void Awake()
+    {
+        bar.value = (float)curHP / (float)maxHP;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        imsi = (float)curHP/ (float) maxHP;
+        HandleHp();
+    }
+
+    private void HandleHp()
+    {
+        bar.value = Mathf.Lerp(bar.value, imsi, Time.deltaTime * 10);
+    }
+
+    public void WaterSprayed()
+    {
+        
+    }
+}
