d46c67cf8414644853606b1aef8a739d6e316d6c
박석환 <64780243+parkseokhwan0611@users.noreply.github.com>
2024-12-08 14:35:39 +0900

    Seokhwan/EnemyHpBar

---
+++ Assets/Seokhwan/Scripts/EnemyHpBar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHpBar : MonoBehaviour
+{
+    public MonsterBehavior monsterBehavior;
+    public Image hpBar;
+    public GameObject background;
+    public float hpAmount;
+    private float currentHpFill;
+    private Quaternion fixedRotation;
+    public float changeSpeed = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fixedRotation = transform.rotation;
+        currentHpFill = monsterBehavior.hp / monsterBehavior.maxHp;
+    }
+    void LateUpdate() {
+        //transform.rotation = fixedRotation;
+    }
+    void Awake() {
+        hpAmount = monsterBehavior.GetComponent<MonsterBehavior>().hp;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation = fixedRotation;
+        //background.transform.rotation = fixedRotation;
+    }
+    void HpChange() {
+        hpAmount = monsterBehavior.hp;
+        float targetFill = hpAmount /monsterBehavior.maxHp;
+        currentHpFill = Mathf.MoveTowards(currentHpFill, targetFill, changeSpeed * Time.deltaTime);
+        hpBar.fillAmount = currentHpFill;
+    }
+}
